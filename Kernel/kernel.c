091926e01aa00f2ba5022772f03f48a6a8f2a976
jtorreguitar <jtorreguitar@itba.edu.ar>
2018-05-24 12:59:57 -0300

    I just don't know anymore

--- Kernel/kernel.c
+++ Kernel/kernel.c
@@ -18,7 +18,7 @@ extern uint8_t endOfKernel;
 
 static const uint64_t PageSize = 0x1000;
 
-static void * const userMainAddress = (void*)0x400000;
+static void * const sampleCodeModuleAddress = (void*)0x400000;
 static void * const sampleDataModuleAddress = (void*)0x500000;
 
 typedef int (*EntryPoint)();
@@ -92,6 +92,6 @@ void * initializeKernelBinary()
 int main()
 {	
 	
-	((EntryPoint)userMainAddress)();
+	((EntryPoint)sampleCodeModuleAddress)();
 	return 0;
 }
