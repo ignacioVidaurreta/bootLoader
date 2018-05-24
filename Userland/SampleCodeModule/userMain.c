48004fe0c7e3b308d1d311f349969d8dd151f46f
jtorreguitar <jtorreguitar@itba.edu.ar>
2018-05-24 12:28:31 -0300

    agregada función main para userland

---
+++ Userland/SampleCodeModule/userMain.c
@@ -0,0 +1,15 @@
+//This is the program that will be called from the kernel to begin the execution
+//of any and all user programs. All communication from this user space to kernel
+//space should occur through systemCalls which are clearly defined at
+//Kernel/include/sysCalls.h
+
+
+
+
+
+
+int main() {
+	//the following code is here merely as a test to see if this function is indeed called
+	//anything may be added or removed from here.	
+	return 0xDEADC0DE
+}
