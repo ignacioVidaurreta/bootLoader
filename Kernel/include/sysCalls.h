aed539b93cc1a1a925826c7947e06b676159c142
jtorreguitar <jtorreguitar@itba.edu.ar>
2018-05-28 18:14:51 -0300

    agregada documentación para sysCalls

--- Kernel/include/sysCalls.h
+++ Kernel/include/sysCalls.h
@@ -4,34 +4,35 @@
 #include <stdint.h>
 #include <keyboard.h>
 
-/* ---------------------------------ATTENTION-------------------------------------------
-
-
-USR_OUT, USR_ERR and SYS_CLR_USR have been deprecated, now whenever one writes to the screen
-it is assumed you are writting to the command line (last line) unless specified otherwise.
-
-
-
-------------------------------THANK YOU FOR YOUR TIME--------------------------------*/
-
 //Sys call IDs:
 #define SYS_READ 1			
-#define SYS_WRITE 2			
-#define SYS_CLR_USR 3		//DEPRECATED: now whenever you write to the screen it assumes its the last line unless expressed otherwise
-#define SYS_CLR_SCRN 4		//fills all the screen, except for the command line with blank spaces.
-#define SYS_READ_KEYA 5		//reads the totality of the keyboard buffer. Returns in rax the total ammount of characters read.
-#define SYS_TIME 6 			//returns data related to time. Information expanded in the arguements section.
-#define SYS_BEEP 7			//makes the pc speaker beep. arg1: frequency (1000 is recommended if you just want a normal beep).
-#define SYS_NO_BEEP 8		//makes the pc speaker stop beeping.
-#define SYS_SCRN_INFO 9		//returns information on the screen, such as its width or height, or the width or height of a particular character.
-#define SYS_DEL_CHAR 10		//deletes the last user character.
-#define SYS_DRAW_PXL 11		//draws a pixel at the specified location and with the specified colour.
+#define SYS_WRITE 2
+#define SYS_CLR_SCRN 3		//fills all the screen, except for the command line with blank spaces.
+#define SYS_READ_KEYA 4		//reads the totality of the keyboard buffer. Returns in rax the total ammount of characters read.
+#define SYS_TIME 5 			//returns data related to time. Information expanded in the arguements section.
+#define SYS_BEEP 6			//makes the pc speaker beep. arg1: frequency (1000 is recommended if you just want a normal beep).
+#define SYS_NO_BEEP 7		//makes the pc speaker stop beeping.
+#define SYS_SCRN_INFO 8		//returns information on the screen, such as its width or height, or the width or height of a particular character.
+#define SYS_DEL_CHAR 9		//deletes the last user character.
+#define SYS_DRAW_PXL 10		//draws a pixel at the specified location and with the specified colour. Information expanded in the arguements section.
+
+
+
+
+
+
 
 //arguements for read: 1 file descriptor
 //					   2 buffer to be read into
 //					   3 ammount of characters to be read
 //file descriptors: 1 keyboard buffer as defined in keyboard.c.
 #define KEY_BUF 1
+
+
+
+
+
+
 
 //arguements for write: 1 file descriptor.
 //					    2 buffer to be written from.
@@ -41,6 +42,11 @@ it is assumed you are writting to the command line (last line) unless specified 
 //					3 user error. The command line will display a red message.
 #define STD_OUT 1
 #define STD_ERR 2
+
+
+
+
+
 
 //possible arguements for calling the time sysCall, all of them received as arg1
 //please be aware that the information is returned in greenwich mean time, which
@@ -54,6 +60,11 @@ it is assumed you are writting to the command line (last line) unless specified 
 #define MONTHS 7		//returns months since the current year began.
 #define YRS 8			//returns years since the current birth of christ (I ain't rewriting this for the second coming).
 
+
+
+
+
+
 //possible arguements for calling the SCRN_INFO sysCall, all of them received as arg1
 //all of these are measured in pixels.
 #define WIDTH 1			//width of the screen.
@@ -61,11 +72,23 @@ it is assumed you are writting to the command line (last line) unless specified 
 #define CWIDTH 3		//width of a character.
 #define CHEIGHT 4		//height of a character.
 
+
+
+
+
+
 //arguements for the DRAW_PXL sysCall. arg1: x position, distance in pixels from the left edge of the screen of the pixel to draw.
 //									   arg2: y position, distance in pixels from the top edge of the screen of the pixel to draw.
 //									   arg3: red, intensity of the color red in the pixel.
 //									   arg4: green, intensity of the color green in the pixel.
 //									   arg5: blue, intensity of the color blue in the pixel.
+//please be aware colors must be of an 8 bit value, that is, they should go from 0 to 0xFF. 
+//For example passing red, green and blue as 0, 0, 0 will paint the pixel black. Passing them as 0xFF, 0xFF, 0xFF will create a white pixel.
+
+
+
+
+
 
 
 //the following arguements are received in order according to C 64 bit calling convention.
