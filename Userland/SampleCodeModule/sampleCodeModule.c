5b2b036a20039f8cf09633431df9329f274e99f8
ignacioVidaurreta <ignacioVidaurreta@users.noreply.github.com>
2018-05-30 17:55:14 -0300

    Update

--- Userland/SampleCodeModule/sampleCodeModule.c
+++ Userland/SampleCodeModule/sampleCodeModule.c
@@ -1,3 +1,4 @@
+#include <test.h>
 //This is the program that will be called from the kernel to begin the execution
 //of any and all user programs. All communication from this user space to kernel
 //space should occur through systemCalls which are clearly defined at
@@ -10,7 +11,12 @@
 
 int main() {
 	//the following code is here merely as a test to see if this function is indeed called
-	//anything may be added or removed from here.	
-	printf("Hola/n");
+	//anything may be added or removed from here.
+	char * str;
+	char * aux= "hola";
+	strcpy(str, aux);
+	scanf("%s", str);
+	printf("%s", str);
 	return 0xDEADC0DE;
 }
+
