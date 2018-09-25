c14e573a7ca9d317e7536a516d058489aa5c7cf7
Naco <igvidaurreta@gmail.com>
2018-09-25 19:34:37 -0300

    Mutexes + Tests

--- Kernel/include/mutex.h
+++ Kernel/include/mutex.h
@@ -9,7 +9,7 @@
 
 typedef struct proc_node{
   struct proc_node * next;
-  struct process * p;
+  uint64_t pid;
 }tproc_node;
 
 typedef struct proc_list{
@@ -17,7 +17,7 @@ typedef struct proc_list{
 }tproc_list;
 
 typedef struct mutex{
-  tproc_list * waiting_processes;
+  tproc_list * waitingPIDs;
   char * id;
   uint32_t status;
   uint64_t ownerpid;
@@ -25,22 +25,45 @@ typedef struct mutex{
 
 typedef struct mutex_node{
   tmutex * mutex;
-  tmutex * next;
+  struct mutex_node * next;
 }tmutex_node;
 
 typedef struct motex_list{
   tmutex_node * head;
 }tmutex_list;
 
+//Implementado en asm/mutex.asm
+/*
+ *  Setea el estado a locked.
+ *  En caso de que hubiese estado lockeado
+ *  devuelve 1, devuelve 0 si no estaba lockeado
+ */
+uint32_t mutex_lock(uint32_t *status);
 
 void initMutex();
+int createMutex(char* mutexId, uint64_t processId);
+void addMutex(tmutex_node* node, tmutex* mutex);
+
+int lock(char* mutexId, uint64_t processId);
+int unlock(char* mutexId, uint64_t processId);
+int lockIfUnlocked(char* mutexId, uint64_t processId);
+
+int terminateMutex(char* mutexId, uint64_t processId);
+tmutex* containMutex(tmutex_node* node, char* mutexId);
+void addWaitingProc(int pid, tproc_node* node);
+void removeMutex(char* mutexId, tmutex* mutex);
+void removeAndFreeWaitingProc(tproc_node* node);
+tmutex_node * removeAndFreeFromMutexes(char* mutexId, tmutex_node* node);
 
 
 // TESTS
 void initMutexTest();
-void initMutipleTimesTest();
-
-
+void createMutexCreatesAMutexTest();
+void lockofLockedMutexClaimsMutexTest();
+void lockOfLockedMutexAddsToWaitingListTest();
+void unlockOfLockedMutexChangesOwnerTest();
+void unlockWithoutWaitingChangesStatusToUnlockTest();
+void terminateMutexEliminatesTheMutexTest();
 
 
 #endif
--- Kernel/mutex.c
+++ Kernel/mutex.c
@@ -3,16 +3,16 @@
 #include "naiveConsole.h"
 #include "mutex.h"
 #include "buddy.h"
+#include "lib.h"
 #include "process.h"
 
 
 static tmutex_list * mutexes;
-static int initialized;
-void initMutex(){
 
+void initMutex(){
+  static int initialized;
   if(initialized==0){
     mutexes = mymalloc(sizeof(tmutex_list));
-    mutexes->head = NULL;
 
     tmutex * mutex_master = mymalloc(sizeof(tmutex));
     mutex_master->id = MUTEX_MASTER_ID;
@@ -22,7 +22,7 @@ void initMutex(){
     tproc_list * waiting_proc =  mymalloc(sizeof(tproc_list));
     waiting_proc->head = NULL;
 
-    mutex_master->waiting_processes = waiting_proc;
+    mutex_master->waitingPIDs = waiting_proc;
 
     tmutex_node * node_master = mymalloc(sizeof(tmutex_node));
     node_master->mutex= mutex_master;
@@ -34,8 +34,207 @@ void initMutex(){
   }
 }
 
+int createMutex(char* mutexId, uint64_t processId){
+    lock(MUTEX_MASTER_ID, processId);
+    if (containMutex(mutexes->head, mutexId)!= NULL){ //Ya existe el mutex
+        unlock(MUTEX_MASTER_ID, processId);
+        return -1;
+    }
 
 
+
+    tmutex* mutex = mymalloc(sizeof(tmutex));
+    mutex->id = mutexId;
+    mutex->status = UNLOCKED;
+    mutex->ownerpid = NULL_PID;
+
+    tproc_list* waitList = mymalloc(sizeof(tproc_list));
+    waitList->head = NULL;
+    mutex->waitingPIDs = waitList;
+
+    addMutex(mutexes->head ,mutex);
+    unlock(MUTEX_MASTER_ID, processId);
+
+    return 1;
+}
+
+void addMutex(tmutex_node* node,tmutex* mutex){
+    if (node->next == NULL){
+        tmutex_node * new_node = mymalloc(sizeof(tmutex_node));
+        new_node->mutex = mutex;
+        new_node-> next = NULL;
+        node->next = new_node;
+        return;
+    }
+    addMutex(node->next, mutex);
+}
+
+int lock(char* mutexId, uint64_t processId){
+    if (mutexes == NULL || mutexes->head == NULL){
+        return -1;
+    }
+    tmutex* mutex = containMutex(mutexes->head, mutexId);
+    if (mutex == NULL){
+        return -1;
+    }
+
+    int was_locked= mutex_lock(&mutex->status);
+
+
+    if( was_locked){ //Si estaba bloqueado ==> agregarlo a la cola de procesos esperando
+        if (mutex->waitingPIDs->head == NULL){
+            tproc_node* newProc = mymalloc(sizeof(tproc_node));
+            newProc->next = NULL;
+            newProc->pid = processId;
+            mutex->waitingPIDs->head = newProc;
+            //sleep(processId)
+        }else{
+            addWaitingProc(processId, mutex->waitingPIDs->head);
+            //sleep(processId)
+        }
+
+    }else{
+        mutex->ownerpid = processId;
+    }
+
+
+    return 1;
+}
+
+int unlock(char* mutexId, uint64_t processId){
+    if (mutexes == NULL || mutexes->head == NULL){
+        return -1;
+    }
+    tmutex* mutex = containMutex(mutexes->head, mutexId);
+    if (mutex == NULL){
+        return -1;
+    }
+
+    if( mutex->ownerpid == processId){
+        uint64_t new_pid;
+        int blockedProcessFound = 0;
+
+        //El blockedProcessFound=1 no debería ir, pero hasta que tengamos
+        // la conexión a procesos lo dejo para que agarre el siguietne
+
+        while(!blockedProcessFound && mutex->waitingPIDs->head != NULL){
+            new_pid = mutex->waitingPIDs->head->pid;
+            mutex->waitingPIDs->head = mutex->waitingPIDs->head-> next;
+            //blockedProcessFound = isBlocked(pid);
+            blockedProcessFound = 1; //
+        }
+
+
+        /*
+         *  No hace falta desbloquear el mutex si se
+         *  lo voy a dar a otro proceso. Con simplemente
+         *  cambiar el ownerpid le doy el control del
+         *  mutex entonces sólo el lo puede desbloquearlo
+         */
+         if (blockedProcessFound) {
+             mutex-> ownerpid = new_pid;
+             //wakeup(new_pid);
+         }else{ //Si no encontré proceso, librero el mutex
+             mutex-> status = UNLOCKED;
+         }
+
+         return 1;
+    }
+
+    return -1;
+
+}
+
+int lockIfUnlocked(char * mutexId, uint64_t processId){
+    if (mutexes == NULL || mutexes->head == NULL){
+        return -1;
+    }
+    tmutex* mutex = containMutex(mutexes->head, mutexId);
+    if (mutex == NULL){
+        return -1;
+    }
+    int was_locked= mutex_lock(&mutex->status);
+
+    if (!was_locked){
+        mutex->ownerpid = processId;
+        return 1; //Puede ser lockeado
+    }
+    return 0; //No puede ser lockeado
+}
+
+int terminateMutex(char * mutexId, uint64_t processId){
+    lock(MUTEX_MASTER_ID, processId); //Para que el borrado sea atómico
+
+    tmutex* mutex = containMutex(mutexes->head, mutexId);
+    if (mutex == NULL){ //No existe el mutex
+        unlock(MUTEX_MASTER_ID, processId);
+        return -1;
+    }
+
+    removeMutex(mutexId, mutex);
+    unlock(MUTEX_MASTER_ID, processId);
+    return 1;
+
+}
+tmutex* containMutex(tmutex_node* node, char* mutexId){
+    if (strcmp(node->mutex->id, mutexId) == 0){
+        return node->mutex;
+    }
+    if (node->next == NULL){
+        return NULL;
+    }
+    return containMutex(node->next, mutexId);
+}
+
+void addWaitingProc(int pid, tproc_node* node){
+    if (node->next == NULL){
+        tproc_node* newProc = mymalloc(sizeof(tproc_node));
+        newProc->next = NULL;
+        newProc->pid = pid;
+        node->next = newProc;
+        return;
+    }
+    addWaitingProc(pid, node->next);
+    return;
+}
+
+void removeMutex(char * mutexId, tmutex * mutex){
+    if (mutex->waitingPIDs->head != NULL){
+            removeAndFreeWaitingProc(mutex->waitingPIDs->head);
+    }
+    myfree((void*)mutex->waitingPIDs, sizeof(tproc_list));
+
+    removeAndFreeFromMutexes(mutexId, mutexes->head);
+}
+
+void removeAndFreeWaitingProc(tproc_node* node){
+    if (node == NULL){
+        return;
+    }
+    removeAndFreeWaitingProc(node->next);
+    myfree(node, sizeof(tproc_node));
+}
+
+tmutex_node* removeAndFreeFromMutexes(char * mutexId, tmutex_node* node){
+    if (node == NULL){
+        /*
+         *  NUNCA debería entrar acá, porque sólo entaría si el mutex
+         *  no estuviese en la lista de mutexes, lo cual ya checkié que está
+         *  pero bueno, por las dudas de que lo vaya a querer usar en otro
+         *  contexto lo dejo
+         */
+        return NULL;
+    }
+    if (strcmp(node->mutex->id, mutexId) == 0){
+        tmutex_node* aux_node = node->next;
+        myfree(node->mutex, sizeof(tmutex));
+        myfree(node, sizeof(tmutex_node));
+        return aux_node;
+    }
+
+    node->next= removeAndFreeFromMutexes(mutexId, node->next);
+    return node;
+}
 
 
 
@@ -58,23 +257,101 @@ void initMutexTest(){
     ncPrint("initMutexTest FAILED! : mutexes->head = NULL");
   else if(mutexes->head->mutex == NULL)
     ncPrint("initMutexTest FAILED! : mutexes->head->mutex = NULL");
-  else if(mutexes->head->mutex->waiting_processes == NULL)
-    ncPrint("initMutexTest FAILED! : mutexes->head->mutex->waiting_processes = NULL");
+  else if(mutexes->head->mutex->waitingPIDs == NULL)
+    ncPrint("initMutexTest FAILED! : mutexes->head->mutex->waitingPIDs = NULL");
   else
     ncPrint("initMutexTest PASSED!");
 }
-/*
-void initMutipleTimesTest(){
-  if ( initialized != 0){
-    ncPrint("initMutipleTimesTest FAILED: Initialized = 0");
-  }
-  else{
-    initMutex();
-    if (initialized >1){
-      ncPrint("initMutipleTimesTest FAILED: Initialized >1");
-    }else{
-      ncPrint("initMutipleTimesTest PASSED!");
+void createMutexCreatesAMutexTest(){
+    int value = createMutex("HOLA_SOY_UN_TEST", 1);
+    if (value == -1){
+        ncPrint("ERROR: createMutex Failed");
+        return;
     }
-  }
+    tmutex_node* aux = mutexes->head;
+    int found = 0;
+    while(aux != NULL && !found){
+        if (strcmp("HOLA_SOY_UN_TEST", aux->mutex->id) == 0){
+            found =1;
+            ncPrint("createMutexCreatesAMutexTest PASSED! ");
+        }
+        aux = aux->next;
+    }
+    if (!found){
+        ncPrint("createMutexCreatesAMutexTest FAILED! ");
+    }
 }
-*/
+
+void lockofLockedMutexClaimsMutexTest(){
+    createMutex("LOCKEO_EL_MUTEX", 2);
+    lock("LOCKEO_EL_MUTEX", 2);
+
+    tmutex * mutex = containMutex(mutexes->head, "LOCKEO_EL_MUTEX");
+    if(mutex == NULL){
+        ncPrint("TEST ERROR: Couldn't find mutex");
+    }
+    if (mutex->ownerpid == 2){
+        ncPrint("lockofLockedMutexClaimsMutexTest PASSED!");
+    }else{
+        ncPrint("lockofLockedMutexClaimsMutexTest FAILED!");
+    }
+
+    return;
+
+}
+
+void lockOfLockedMutexAddsToWaitingListTest(){
+
+    lock("LOCKEO_EL_MUTEX", 3);
+
+    tmutex * mutex = containMutex(mutexes->head, "LOCKEO_EL_MUTEX");
+    if(mutex == NULL){
+        ncPrint("TEST ERROR: Couldn't find mutex");
+    }
+    if (mutex->waitingPIDs->head != NULL && mutex->waitingPIDs->head->pid == 3){
+        ncPrint("lockOfLockedMutexAddsToWaitingListTest: PASSED!");
+    }else{
+        ncPrint("lockOfLockedMutexAddsToWaitingListTest: FAILED!");
+    }
+
+}
+
+void unlockOfLockedMutexChangesOwnerTest(){
+    unlock("LOCKEO_EL_MUTEX", 2);
+    tmutex * mutex = containMutex(mutexes->head, "LOCKEO_EL_MUTEX");
+    if(mutex == NULL){
+        ncPrint("TEST ERROR: Couldn't find mutex");
+    }
+
+    if (mutex->ownerpid == 3){
+        ncPrint("unlockOfLockedMutexChangesOwnerTest: PASSED!");
+    }else{
+        ncPrint("unlockOfLockedMutexChangesOwnerTest: FAILED!");
+    }
+}
+
+void unlockWithoutWaitingChangesStatusToUnlockTest(){
+    unlock("LOCKEO_EL_MUTEX", 3);
+
+    tmutex * mutex = containMutex(mutexes->head, "LOCKEO_EL_MUTEX");
+    if(mutex == NULL){
+        ncPrint("TEST ERROR: Couldn't find mutex");
+    }
+
+    if( mutex->status == UNLOCKED){
+        ncPrint("unlockWithoutWaitingChangesStatusToUnlockTest: PASSED!");
+    }else{
+        ncPrint("unlockWithoutWaitingChangesStatusToUnlockTest: FAILED!");
+    }
+}
+
+void terminateMutexEliminatesTheMutexTest(){
+    terminateMutex("LOCKEO_EL_MUTEX", 4);
+
+    tmutex * mutex = containMutex(mutexes->head, "LOCKEO_EL_MUTEX");
+    if(mutex == NULL){
+        ncPrint("terminateMutexEliminatesTheMutexTest: PASSED!");
+    }else{
+        ncPrint("terminateMutexEliminatesTheMutexTest: FAILED!");
+    }
+}
