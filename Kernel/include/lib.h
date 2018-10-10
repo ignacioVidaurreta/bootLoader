3832e3cbdf0d0241d8486151c5bd67d8b379ade9
Naco <igvidaurreta@gmail.com>
2018-10-10 12:48:18 -0300

    Layout of philosophers
    !!! DOESNT WORK YET !!!

--- Kernel/include/lib.h
+++ Kernel/include/lib.h
@@ -10,6 +10,8 @@ char* strcpy(char* dest,const char* src);
 int strcmp(const char* str1,const char* str2);
 int strlen(const char* str);
 char* concat(const char* str1, const char* str2);
+char * intToString(int num, char * numStr);
+void swap(char* numStr, int index1, int index2);
 
 char *cpuVendor(char *result);
 
