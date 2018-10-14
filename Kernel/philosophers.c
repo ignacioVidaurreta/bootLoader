44d42c55e88a3b44268c6f85fd041b5db95a70e6
Naco <igvidaurreta@gmail.com>
2018-10-14 17:42:07 -0300

    Upgrade Philosophers
    Doesn't work perfectly yet

--- Kernel/philosophers.c
+++ Kernel/philosophers.c
@@ -8,66 +8,82 @@
 
 
 void philosophers(uint64_t i){
-    ncScroll(); /*Por qué funciona sólo si está arriba?? ._. */
-    ncPrint((char*)i);
+    // ncScroll(); /*Por qué funciona sólo si está arriba?? ._. */
+    // ncPrint((char*)i);
+    int num = *((char*)i) - '0';
+    char * name =get_current_proc()->name;
+    uint64_t pid = get_current_proc()->pid;
 
-    /*
-        char * name =get_current_proc()->name;
-        uint64_t pid = get_current_proc()->pid;
+    char* right_fork = mymalloc(sizeof(right_fork));
+    char* left_fork = mymalloc(sizeof(left_fork));
 
-        char* right_fork = mymalloc(sizeof(right_fork));
-        char* left_fork = mymalloc(sizeof(left_fork));
+    intToString((num+1)%5, right_fork);
+    intToString(num, left_fork);
 
-        intToString((i+1)%5, right_fork);
-        intToString(i, left_fork);
-
-        ncPrint(name);
-        ncPrint("  is Waiting for his or hers");
-        if ( i%2 == 0){ //Si es par
-            ncPrint("left fork");
-            ncScroll();
-            lock(concat("__FORK", right_fork), pid);
-            ncPrint("right fork");
-            ncScroll();
-            lock(concat("__FORK", left_fork), pid);
-        }else{
-            ncPrint("right fork");
-            ncScroll();
-            lock(concat("__FORK", left_fork), pid);
-            ncPrint("left fork");
-            ncScroll();
-            lock(concat("__FORK", right_fork), pid);
-        }
-        ncPrint(name);
-        ncPrint("  is Eating");
-        int j = 0;
-        while(j<100000){
-            j++;
-        }
-        ncPrint(name);
-        ncPrint(" stopped eating and is releasing ");
-        if ( i%2 == 0){ //Si es par
-            ncPrint("left fork");
-            ncScroll();
-            unlock(concat("__FORK", right_fork), pid);
-            ncPrint("right fork");
-            ncScroll();
-            unlock(concat("__FORK", left_fork), pid);
-        }else{
-            ncPrint("right fork");
-            unlock(concat("__FORK", left_fork), pid);
-            ncPrint("left fork");
-            unlock(concat("__FORK", right_fork), pid);
-        }
-
-        ncPrint(name);
-        ncPrint("  is thinking ...");
+    lock("__PRINT_MUTEX__", pid);
+    ncPrint(name);
+    ncPrint(" is waiting for the ");
+    if ( i%2 == 0){ //Si es par
+        ncPrint("left fork... ");
         ncScroll();
-        j = 0;
-         while(j<100000){
-            j++;
-        }
-      */
+        unlock("__PRINT_MUTEX__", pid);
+        lock(concat("__FORK", left_fork), pid);
+        lock("__PRINT_MUTEX__", pid);
+        ncPrint(name);
+        ncPrint(" is waiting for the right fork... ");
+        ncScroll();
+        unlock("__PRINT_MUTEX__", pid);
+        lock(concat("__FORK", right_fork), pid);
+    }else{
+        ncPrint("right fork...");
+        ncScroll();
+        unlock("__PRINT_MUTEX__", pid);
+        lock(concat("__FORK", right_fork), pid);
+        lock("__PRINT_MUTEX__", pid);
+        ncPrint(name);
+        ncPrint(" is waiting for the left fork...");
+        ncScroll();
+        unlock("__PRINT_MUTEX__", pid);
+        lock(concat("__FORK", left_fork), pid);
+    }
+    lock("__PRINT_MUTEX__", pid);
+    ncPrint(name);
+    ncPrint(" is eating");
+    ncScroll();
+    unlock("__PRINT_MUTEX__", pid);
+
+
+    lock("__PRINT_MUTEX__", pid);
+    ncPrint(name);
+    ncPrint(" stopped eating and is releasing the ");
+    if ( i%2 == 0){ //Si es par
+        ncPrint("left fork... ");
+        ncScroll();
+        unlock("__PRINT_MUTEX__", pid);
+        unlock(concat("__FORK", left_fork), pid);
+        lock("__PRINT_MUTEX__", pid);
+        ncPrint(name);
+        ncPrint(" is releasing the right fork...");
+        ncScroll();
+        unlock("__PRINT_MUTEX__", pid);
+        unlock(concat("__FORK", right_fork), pid);
+    }else{
+        ncPrint("right fork... ");
+        ncScroll();
+        unlock("__PRINT_MUTEX__", pid);
+        unlock(concat("__FORK", right_fork), pid);
+        lock("__PRINT_MUTEX__", pid);
+        ncPrint(name);
+        ncPrint(" is releasing the left fork");
+        ncScroll();
+        unlock("__PRINT_MUTEX__", pid);
+        unlock(concat("__FORK", left_fork), pid);
+    }
+    lock("__PRINT_MUTEX__", pid);
+    ncPrint(name);
+    ncPrint("is thinking ...");
+    ncScroll();
+    unlock("__PRINT_MUTEX__", pid);
 
 }
 
@@ -81,12 +97,14 @@ void initPhil(){
         createMutex(concat("__FORK", intToString(i, aux)), 20); //Ese 20 es medio rancio
     }
 
+    createMutex("__PRINT_MUTEX__", get_current_proc()->pid);
     //char* args[] = {"0", "1", "2", "3", "4"};
     char * cero[] = {"0"};
     char * uno[] = {"1"};
-    char * dos[] = {"2"};
-    char * tres[] = {"3"};
-    char * cuatro[] = {"4"};
+    // char * dos[] = {"2"};
+    // char * tres[] = {"3"};
+    // char * cuatro[] = {"4"};
+    //int64_t pids[5];
     for(int j = 0; j<5; j++){
         // ncPrint(args[j]);
         // ncScroll();
@@ -98,21 +116,23 @@ void initPhil(){
             case 1:
                 start_proc("Socrates", (void*) philosophers, 1, uno );
                 break;
-            case 2:
-                start_proc("Platon", (void*) philosophers, 1, dos);
-                break;
-            case 3:
-                start_proc("Simone de Beauvoir", (void*) philosophers, 1, tres);
-                break;
-            case 4:
-                start_proc("Kant", (void*) philosophers, 1, cuatro);
-                break;
+            // case 2:
+            //     pids[2]= start_proc("Platon", (void*) philosophers, 1, dos);
+            //     break;
+            // case 3:
+            //     pids[3]= start_proc("Simone de Beauvoir", (void*) philosophers, 1, tres);
+            //     break;
+            // case 4:
+            //     pids[4]= start_proc("Kant", (void*) philosophers, 1, cuatro);
+            //     break;
             // // default:
             //     start_proc(concat("Generic #", intToString(j, aux2)), (void*) philosophers, 1, args); //por si llegamos a ampliar
             //     break;
         }
 
     }
+
+
 
 
 }
